30e9d9cd10da3442a6fd6b357c538800482b7d1a
Ken Nichol <1321022+kennithnichol@users.noreply.github.com>
2018-10-11 09:30:17 -0700

    Update precache-manifest.f0ec0a988cb65a9b0c15f3c0cc2203bd.js

--- precache-manifest.f0ec0a988cb65a9b0c15f3c0cc2203bd.js
+++ precache-manifest.f0ec0a988cb65a9b0c15f3c0cc2203bd.js
@@ -1,30 +1,30 @@
 self.__precacheManifest = [
   {
     "revision": "12a3db8ba7f5423327bb12aed3c41977",
-    "url": "/ZtM-robofriends/static/media/SEGA.12a3db8b.woff"
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
   },
   {
     "revision": "0faf84f5a4d47c0179f2",
-    "url": "/ZtM-robofriends/static/js/runtime~main.0faf84f5.js"
+    "url": "/robofriends/static/js/runtime~main.0faf84f5.js"
   },
   {
     "revision": "92091383c1b7fb37ca28",
-    "url": "/ZtM-robofriends/static/js/main.92091383.chunk.js"
+    "url": "/robofriends/static/js/main.92091383.chunk.js"
   },
   {
     "revision": "1c3193fd3ff2fab03325",
-    "url": "/ZtM-robofriends/static/js/1.1c3193fd.chunk.js"
+    "url": "/robofriends/static/js/1.1c3193fd.chunk.js"
   },
   {
     "revision": "92091383c1b7fb37ca28",
-    "url": "/ZtM-robofriends/static/css/main.5606e5e7.chunk.css"
+    "url": "/robofriends/static/css/main.5606e5e7.chunk.css"
   },
   {
     "revision": "1c3193fd3ff2fab03325",
-    "url": "/ZtM-robofriends/static/css/1.5cd03d96.chunk.css"
+    "url": "/robofriends/static/css/1.5cd03d96.chunk.css"
   },
   {
     "revision": "4c5c39a2d56ba8c934b40f3e2e9bea33",
-    "url": "/ZtM-robofriends/index.html"
+    "url": "/robofriends/index.html"
   }
 ];
