990ccd238f566b375ad5c624593af43c7923b769
Ken Nichol <1321022+kennithnichol@users.noreply.github.com>
2018-10-13 21:55:11 -0700

    hide <label> contents

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -3,6 +3,7 @@ import React from 'react';
 const SearchBox = ({ searchfield, searchChange }) => {
 	return (
 		<div className="pa2">
+			<label className="dn">Filter robofriends:</label>
 			<input
 				className="pa3 ba b--green bg-lightest-blue "
 				type="search"
