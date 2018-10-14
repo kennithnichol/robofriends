13c006287459d3710bb1b56a77cc59ace0c2ad4f
Ken Nichol <1321022+kennithnichol@users.noreply.github.com>
2018-10-13 22:33:52 -0700

    Wrap search box with a <label> for accessibility

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -3,13 +3,15 @@ import React from 'react';
 const SearchBox = ({ searchfield, searchChange }) => {
 	return (
 		<div className="pa2">
-			<label className="dn">Filter robofriends:</label>
-			<input
-				className="pa3 ba b--green bg-lightest-blue "
-				type="search"
-				placeholder="Search robots"
-				onChange={searchChange}
-			/>
+			<label>
+				<span className="dn">Filter robofriends</span>
+				<input
+					className="pa3 ba b--green bg-lightest-blue "
+					type="search"
+					placeholder="Search robots"
+					onChange={searchChange}
+				/>
+			</label>
 		</div>
 	)
 }
