af968237d08fcfd36ed2915a395f968c5ee742d8
Ken Nichol <1321022+kennithnichol@users.noreply.github.com>
2018-10-13 23:00:42 -0700

    convert to propert `aria-label` for accessibility

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -2,16 +2,14 @@ import React from 'react';
 
 const SearchBox = ({ searchfield, searchChange }) => {
 	return (
-		<div className="pa2">
-			<label>
-				<span className="dn">Filter robofriends</span>
-				<input
-					className="pa3 ba b--green bg-lightest-blue "
-					type="search"
-					placeholder="Search robots"
-					onChange={searchChange}
-				/>
-			</label>
+		<div className="pa2">			
+			<input
+				aria-label="search robots"
+				className="pa3 ba b--green bg-lightest-blue "
+				type="search"
+				placeholder="Search robots"
+				onChange={searchChange}
+			/>
 		</div>
 	)
 }
