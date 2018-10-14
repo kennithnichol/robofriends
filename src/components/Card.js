fa16dd8e325709102d56db1ec77e4dd98f34c41e
Ken Nichol <1321022+kennithnichol@users.noreply.github.com>
2018-10-13 23:05:03 -0700

    add size parameter name to robohash api calls

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = ({ name, email, id, username }) => {
 	return (
 		<div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
-			<img src={`https://robohash.org/${username}?200x200`} alt='generated robot' />
+			<img src={`https://robohash.org/${username}?size=200x200`} alt='generated robot' />
 			<div>
 				<h2>{name}</h2>
 				<p>{email}</p>
