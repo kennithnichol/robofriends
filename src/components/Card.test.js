1f0476476f8b5de54b2aec40c1d2ce5ee09b684d
Kennith Nichol <1321022+kennithnichol@users.noreply.github.com>
2018-10-16 15:40:22 -0700

    Initial test for Card component using enzyme

---
+++ src/components/Card.test.js
@@ -0,0 +1,8 @@
+import {shallow} from 'enzyme';
+import React from 'react';
+import Card from './Card';
+
+it('expect to render Card Component', () => {
+	expect(shallow(<Card />).length).toEqual(1)
+})
+
