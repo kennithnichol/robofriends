dec81b6d5419cb8e9101c99468ad042b8dcc134b
Kennith Nichol <1321022+kennithnichol@users.noreply.github.com>
2018-10-16 16:41:55 -0700

    Complete test coverage of <Header />

--- src/components/Header.test.js
+++ src/components/Header.test.js
@@ -2,7 +2,15 @@ import {shallow} from 'enzyme';
 import React from 'react';
 import Header from './Header';
 
-it('expect to render Header Component', () => {
-	expect(shallow(<Header />)).toMatchSnapshot();
+describe('Header component', () => {
+	it('expect to render Header Component', () => {
+		expect(shallow(<Header />)).toMatchSnapshot();
+	})
+
+	it('expect not to update', () => {
+		const wrapper = shallow(<Header />);
+		const instance = wrapper.instance();
+		expect(instance.shouldComponentUpdate()).toEqual(false);
+	})
 })
 
