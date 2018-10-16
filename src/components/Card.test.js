d2ab88e033a1aea7c8c3dc4844aca7659a7eaefb
Kennith Nichol <1321022+kennithnichol@users.noreply.github.com>
2018-10-16 15:51:24 -0700

    Add snapshot test to Card component

--- src/components/Card.test.js
+++ src/components/Card.test.js
@@ -3,6 +3,6 @@ import React from 'react';
 import Card from './Card';
 
 it('expect to render Card Component', () => {
-	expect(shallow(<Card />).length).toEqual(1)
+	expect(shallow(<Card />)).toMatchSnapshot();
 })
 
