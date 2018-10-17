fe445ad3e166f791eb6518a1149e88e71fcd8e5e
Ken Nichol <1321022+kennithnichol@users.noreply.github.com>
2018-10-16 21:32:34 -0700

    test <App /> with export of <App />

--- src/containers/App.test.js
+++ src/containers/App.test.js
@@ -1,6 +1,16 @@
+import { shallow } from 'enzyme';
+import React from 'react';
+import { App } from './App';
 
 describe('App container', () => {
-	it('test', () => {
+	let wrapper;
+	const mockOnSearchChange = jest.fn();
 
+	beforeEach(() => {
+		wrapper = shallow(<App onSearchChange={mockOnSearchChange} />)
+	})
+
+	it('expect to render App component', () => {
+		expect(wrapper).toMatchSnapshot();
 	})
 });
